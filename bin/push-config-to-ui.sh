e95dce5f8a1deea901eb4a3cc78d7075784e9f45
Frank Tamas <sitya@niif.hu>
2015-05-26 10:54:32 +0200

    update

--- bin/push-config-to-ui.sh
+++ bin/push-config-to-ui.sh
@@ -4,15 +4,16 @@ source /var/compostmonitor-daemon/main.conf
 
 for i in `ls $CONFDDIR`;
 do
-  if [[ $i -eq "dht" ]]; then
-  	touch $LOGDIR/ht_$i.txt
-  fi
   while read line; do
+    if [[ "$i" = "dht" ]]; then
+	id=`echo $line | cut -f1 -d: |cut -f2 -d\|`
+	touch /tmp/ht_$id.txt
+    fi
     name=`echo ${line// /_} | awk -F  ":" '{print $2}'`
 
-  	params=`echo $line | awk -F  "|" '{print "{\"type\";\""$1"\", \"localid\";\""$2"\"}" }' | awk -F  ":" '{print  "\""$1"\", \"name\":"}'`
+    params=`echo $line | awk -F  "|" '{print "{\"type\";\""$1"\", \"localid\";\""$2"\"}" }' | awk -F  ":" '{print  "\""$1"\", \"name\":"}'`
     json=${params:1}\"${name:1}\"}
-    curl -X POST -d "${json//;/:}" $COMPOSTSERVER_HOSTNAME/api/v1/sensors.json --header "Content-Type:application/json" --user $COMPOSTSERVER_USERNAME:$COMPOSTSERVER_PASSWORD
-  done < ../conf.d/$i
-  echo "Done."
+    echo curl -X POST -d "${json//;/:}" $COMPOSTSERVER_HOSTNAME/api/v1/sensors.json --header "Content-Type:application/json" --user $COMPOSTSERVER_USERNAME:$COMPOSTSERVER_PASSWORD
+  done < $CONFDDIR/$i
 done
+echo "Done."
