275e4dc9a29509f563e2e726298b54ebc75e00cb
Frank Tamás <sitya@niif.hu>
2015-06-15 10:40:26 +0200

    upd

--- bin/push-config-to-ui.sh
+++ bin/push-config-to-ui.sh
@@ -5,15 +5,10 @@ source /var/compostmonitor-daemon/main.conf
 for i in `ls $CONFDDIR`;
 do
   while read line; do
-    if [[ "$i" = "dht" ]]; then
-	id=`echo $line | cut -f1 -d: |cut -f2 -d\|`
-	touch /tmp/ht_$id.txt
-    fi
     name=`echo ${line// /_} | awk -F  ":" '{print $2}'`
-
     params=`echo $line | awk -F  "|" '{print "{\"type\";\""$1"\", \"localid\";\""$2"\"}" }' | awk -F  ":" '{print  "\""$1"\", \"name\":"}'`
-    json=${params:1}\"${name:1}\"}
-    echo curl -X POST -d "${json//;/:}" $COMPOSTSERVER_HOSTNAME/api/v1/sensors.json --header "Content-Type:application/json" --user $COMPOSTSERVER_USERNAME:$COMPOSTSERVER_PASSWORD
+    json=${params:1}\"${name:1}\",\"user\"\:\"$COMPOSTSERVER_USERNAME\"}
+    curl -X POST -d "${json//;/:}" $COMPOSTSERVER_HOSTNAME/api/v1/sensors.json --header "Content-Type:application/json" --user $COMPOSTSERVER_USERNAME:$COMPOSTSERVER_PASSWORD
+    echo $name "...OK"
   done < $CONFDDIR/$i
 done
-echo "Done."
--- bin/push.sh
+++ bin/push.sh
@@ -2,10 +2,20 @@
 
 source /var/compostmonitor-daemon/main.conf
 
+
 for point in `ls $PROCDIR`
 do
+  #params=`echo $point | awk -F  "." '{print "id=" $1 "&time=" $2 "&value="}'`
   params=`echo $point | awk -F  "." '{print "{\"localid\";\""$1"\",\"timestamp\";\""$2 }'`
   json={${params:1}\",\"value\":\"`cat $PROCDIR/$point`\"}
-  curl -X POST -d "${json//;/:}" $COMPOSTSERVER_HOSTNAME/api/v1/sensorvalues.json --header "Content-Type:application/json" --user $COMPOSTSERVER_USERNAME:$COMPOSTSERVER_PASSWORD
+  trying=0
+  until $(curl -X POST -d "${json//;/:}" --output /dev/null --silent $COMPOSTSERVER_HOSTNAME/api/v1/sensorvalues.json --header "Content-Type:application/json" --user $COMPOSTSERVER_USERNAME:$COMPOSTSERVER_PASSWORD); do
+       if [ $trying -lt 5 ]; then
+          $trying=$trying+1
+          sleep 3
+       else
+          exit 1
+       fi
+  done
   rm $PROCDIR/$point
 done
